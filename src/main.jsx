1d1ee4f8a574967debe2e6c0d8eafd4d6f3bab7e
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-04-22 00:31:22 +0600

    few of changes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import router from './routes/routes.jsx'
+import { RouterProvider } from "react-router";
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Toaster
+      position="bottom-right"
+      reverseOrder={false}
+    />
   </StrictMode>,
+
 )
